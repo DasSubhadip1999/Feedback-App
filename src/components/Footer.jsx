7dc18f009c1cf526c872c8b851cca3b481ab7174
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-09-06 09:45:48 +0530

    anchor tag no referrer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>created with ❤️ by <a href="https://www.linkedin.com/in/subhadip-das-04b48b205" target="_blank">Subhadip Das</a> @2022</footer>
+    <footer>created with ❤️ by <a href="https://www.linkedin.com/in/subhadip-das-04b48b205" target="_blank" rel='noreferrer'>Subhadip Das</a> @2022</footer>
   )
 }
 
