9534e4a3f50aa939718d04a75da4995e1052e757
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-09-06 09:41:44 +0530

    favicon add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>created with ❤️ by <a href="https://www.linkedin.com/in/subhadip-das-04b48b205" target="_blank">Subhadip Das</a> @2022</footer>
+  )
+}
+
+export default Footer
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,19 +1,3 @@
-const FeedbackData = [
-    {
-        id : 1,
-        rating : 9,
-        text : "this is example one"
-    },
-    {
-        id : 2,
-        rating : 8,
-        text : "this is example two"
-    },
-    {
-        id : 3,
-        rating : 10,
-        text : "this is example three"
-    }
-]
+const FeedbackData = [];
 
 export default FeedbackData;
